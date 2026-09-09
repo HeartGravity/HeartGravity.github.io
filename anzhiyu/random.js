ecc45edc622e213e377ae7610e39c7c6822bcb23
Heart <3210929616@qq.com>
2026-09-09 19:00:25 +0800

    Site updated: 2026-09-09 19:00:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/31/GRU4Rec——将RNN融入会话推荐/","2026/08/31/hello-world/"];function toRandomPost(){
+var posts=["2026/08/31/GRU4Rec——将RNN融入会话推荐/","2026/09/09/BERT4Rec——从论文到源码解读/","2026/08/31/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
